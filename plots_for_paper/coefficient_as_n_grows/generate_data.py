9a4a5f787a529e14d551bafbca302456bb8d0923
Itai Pelles <pelles.itai@gmail.com>
2025-01-24 12:44:43 +0200

    properly run all coefficient as n grows data and plots

--- plots_for_paper/coefficient_as_n_grows/generate_data.py
+++ plots_for_paper/coefficient_as_n_grows/generate_data.py
@@ -5,10 +5,10 @@ from tqdm import tqdm
 from joblib import Parallel, delayed
 import sys
 sys.path.append('./')
-from utils.distribution_defs import SIMULATIONS
+from utils.distribution_defs import SIMULATIONS, SIMULATIONS_IN_PAPER
 from utils.methods_defs import INDEP_TESTS
 
-def generate_data_single_dist(dist, dist_name, ns):
+def generate_data_single_dist(dist, dist_name, ns, noise):
     test_instances = {
         key: value() for key,value in INDEP_TESTS.items() if 'hhg' not in key
     }
@@ -25,12 +25,15 @@ def generate_data_single_dist(dist, dist_name, ns):
             key: [] for key in stats.keys()
         }
         for _ in range(reps_per_n-4*i):
-            x,y = dist(n,1)
+            if dist_name in ["multiplicative_noise", "multimodal_independence"]:
+                x,y = dist(n,1)
+            else:
+                x,y = dist(n,1, noise=noise)
             for key, test_instance in test_instances.items():
                 cur_n_stats[key].append(test_instance.statistic(x,y))
         for key in stats.keys():
             stats[key].append(np.mean(cur_n_stats[key]))
-    with open(f'plots_for_paper/coefficient_as_n_grows/data/{dist_name}.pkl', 'wb') as f:
+    with open(f'plots_for_paper/coefficient_as_n_grows/data/{dist_name}_noise_{noise}.pkl', 'wb') as f:
         pickle.dump(stats, f)
 
 def generate_data_for_coefficient_plots(seed):
@@ -41,8 +44,10 @@ def generate_data_for_coefficient_plots(seed):
     np.random.seed(seed)
     Parallel(n_jobs=-1, verbose=100)(
         [
-            delayed(generate_data_single_dist)(dist, dist_name, ns)
+            delayed(generate_data_single_dist)(dist, dist_name, ns, noise)
             for dist_name, dist in SIMULATIONS.items()
+            # for dist_name, dist in SIMULATIONS_IN_PAPER.items()
+            for noise in [0, 0.05, 0.1, 0.2, 0.5]
         ]
     )
 
--- plots_for_paper/coefficient_as_n_grows/generate_plots.py
+++ plots_for_paper/coefficient_as_n_grows/generate_plots.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append('./')
 from utils.matplotlib_helpers import _RCPARAMS_LATEX_SINGLE_COLUMN, save_figure
-from utils.distribution_defs import SIM_TITLES, TEST_LABELS
+from utils.distribution_defs import SIM_TITLES
+from utils.methods_defs import TEST_LABELS
 
-def generate_single_plot(dist_name, dist_title, ns):
-    with open(f'plots_for_paper/coefficient_as_n_grows/data/{dist_name}.pkl', 'rb') as f:
+
+def generate_single_plot(dist_name, dist_title, ns, noise):
+    with open(f'plots_for_paper/coefficient_as_n_grows/data/{dist_name}_noise_{noise}.pkl', 'rb') as f:
         stats = pickle.load(f)
     with plt.rc_context(rc = _RCPARAMS_LATEX_SINGLE_COLUMN):
         for (key, values), marker in zip(stats.items(), ['o', 'd', 's', '*', '>']):
@@ -15,18 +17,19 @@ def generate_single_plot(dist_name, dist_title, ns):
         plt.gca().legend()
         plt.gca().set_ylim(0,1.02)
         plt.gca().set_xscale('log')
-        # plt.xlabel('Sample Size')
-        # plt.ylabel('Average Coefficient of Correlation')
         plt.gca().set_xticks([100,1000,10000])
         plt.xlim(ns[0], ns[-1]+1000)
-        # plt.title(f'{dist_title} Distribution - Coefficients Performance as $n\\rightarrow\\infty$')
-        save_figure(plt.gcf(), f'plots_for_paper/coefficient_as_n_grows/plots/{dist_name}_coefficients.pdf')
+        save_figure(plt.gcf(), f'plots_for_paper/coefficient_as_n_grows/plots/{dist_name}_coefficients_noise_{noise}.pdf')
         plt.clf()
 
 def generate_coefficients_plots():
     ns = np.load('plots_for_paper/coefficient_as_n_grows/data/ns.npy')
     for dist_name, dist_title in SIM_TITLES.items():
-        generate_single_plot(dist_name, dist_title, ns)
+        for noise in [0, 0.05, 0.1, 0.2, 0.5]:
+            try:
+                generate_single_plot(dist_name, dist_title, ns, noise)
+            except Exception as e:
+                print('missing', dist_name, noise, e)
 
 if __name__ == '__main__':
     generate_coefficients_plots()
